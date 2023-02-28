6dd2138ae910a96cbea4b9596120490967ab988b
AY-BAMZ <aybamz181@gmail.com>
2023-02-28 03:18:35 +0100

    header and homepage issue solved

--- src/pages/components/header/header.jsx
+++ src/pages/components/header/header.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import styles from "../../../styles/header.module.css";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <img
+            src="https://res.cloudinary.com/dmixz7eur/image/upload/v1677542356/chike/CMG_st67ia.png"
+            alt=""
+          />
+        </div>
+        <div className={styles.navLinks}>
+          <li className={styles.link}>Home</li>
+          <li className={styles.link}>About</li>
+          <li className={styles.link}>FAQs</li>
+          <li className={styles.link}>Contact</li>
+        </div>
+        <div className={styles.buttons}>
+          <button className={styles.btn}>Login</button>
+          <button>Sign Up</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,24 +1,42 @@
 import React from "react";
 import Header from "../components/header/header";
+import styles from "../../styles/Home.module.css";
+import { HiArrowUpRight } from "react-icons/hi2";
 
 function HomePage() {
   return (
-    <div className="">
+    <div className={styles.homePage}>
       <Header />
-      <div className="container">
-        <div className="content">
-          <div className="row">
-            <div className="col">
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.row}>
+            <div className={styles.colOne}>
               <h1>Create Vouchers & gift, Cashout your Vouchers</h1>
-              <h6>
+              <h5>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
                 mollitia, molestiae quas vel sint commodi repudiandae
                 consequuntur voluptatum laborum
-              </h6>
-              <div className="buttons">
-                <button className="btn">Create Vouchers</button>
-                <button className="btnTwo">Cash a gift</button>
+              </h5>
+              <div className={styles.buttons}>
+                <div className={styles.btn}>
+                  Create Vouchers
+                  <div className={styles.icon}>
+                    <HiArrowUpRight />
+                  </div>
+                </div>
+                <div className={styles.btnTwo}>
+                  Cash a gift
+                  <div className={styles.icon}>
+                    <HiArrowUpRight />
+                  </div>
+                </div>
               </div>
+            </div>
+            <div className={styles.colTwo}>
+              <img
+                src="https://res.cloudinary.com/dmixz7eur/image/upload/v1677536988/chike/CMG_kryufg.png"
+                alt=""
+              />
             </div>
           </div>
         </div>
