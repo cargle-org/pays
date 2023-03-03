5140af17a166038b2fbba2fad25b9dfb0eabb792
AY-BAMZ <aybamz181@gmail.com>
2023-03-03 12:58:01 +0100

    make deposit and verify payment endpoint created withdraw and deposit page issue solved

---
+++ src/pages/voucher/[individualVoucherPage].jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import TopBar from '../components/header/topBar'
+import Sidebar from '../components/sidebar/sidebar'
+
+function IndividualVoucherPage() {
+  return (
+    <div>
+        <Sidebar />
+        <div className="mainBody">
+            <TopBar />
+            <div className="body">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default IndividualVoucherPage
