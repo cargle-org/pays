24d3cd742d25e8497e529d2ef2c3aa0a1ff519d3
AY-BAMZ <aybamz181@gmail.com>
2023-03-03 18:32:24 +0100

    verify payment issue solved

--- src/pages/api/auth/auth.js
+++ src/pages/api/auth/auth.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const checkDomWindow = () => {
+export const checkDomWindow = () => {
   return typeof window !== "undefined";
 };
 
--- src/pages/payment/depositecompleted.jsx
+++ src/pages/payment/depositecompleted.jsx
@@ -2,15 +2,17 @@ import React, { useEffect } from "react";
 import { verifyPayment } from "../api/payment/verifyPayment";
 import TopBar from "../components/header/topBar";
 import Sidebar from "../components/sidebar/sidebar";
+import { useRouter } from 'next/router'
+import styles from "../../styles/components/maindashboardpage.module.css"
+import SuccessMessage from "./successMessage";
 
 function DepositCompleted() {
-  const url = window.location.href;
+  const router = useRouter();
+  const { status } = router.query;
+  const { tx_ref } = router.query;
+  const { transaction_id } = router.query;
+  console.log('status', status, transaction_id, tx_ref)
 
-  const searchParams = new URLSearchParams(new URL(url).search);
-
-  const status = searchParams.get("status");
-  const tx_ref = searchParams.get("tx_ref");
-  const transaction_id = searchParams.get("transaction_id");
 
   useEffect(() => {
     (async () => {
@@ -26,15 +28,7 @@ function DepositCompleted() {
       <div className={styles.mainSection}>
         <TopBar />
         <div className={styles.body}>
-          <div className={styles.successMessage}>
-            <div className={styles.message}>
-              <h5>Your Deposit of ₦500 was successful</h5>
-              <div className={styles.buttons}>
-                <button>Go to wallet</button>
-                <button>Create Voucher</button>
-              </div>
-            </div>
-          </div>
+          <SuccessMessage />
         </div>
       </div>
     </div>
