56c12e40288ced73487a7e7ba7aa9a56379cf2c3
AY-BAMZ <aybamz181@gmail.com>
2023-03-04 15:42:42 +0100

    fixing vouchers page UI

--- src/pages/voucher/[individualVoucherPage].jsx
+++ src/pages/voucher/[individualVoucherPage].jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TopBar from '../components/header/topBar'
 import Sidebar from '../components/sidebar/sidebar'
+import styles from "../../styles/components/maindashboardpage.module.css"
+import VoucherDetailsCard from './VoucherDetailsCard';
+import { useRouter } from "next/router";
+import { getOneVoucher } from '../api/vouchers/getOneVoucher';
 
 function IndividualVoucherPage() {
+  const router = useRouter();
+
+  const id = router.query.individualVoucherPage;
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+
+    (async () => {
+      const res = await getOneVoucher({id});
+      setData(res)
+    })();
+    // setActiveTab(3);
+  }, [id]);
+
   return (
-    <div>
-        <Sidebar />
-        <div className="mainBody">
+    <div className={styles.mainDashboardPage}>
+        <div className={styles.sidebar}>
+            <Sidebar />
+        </div>
+        <div className={styles.mainSection}>
             <TopBar />
-            <div className="body">
-                
+            <div className={styles.body}>
+              <VoucherDetailsCard data={data}/>
             </div>
         </div>
     </div>
