20928f1607dbfe3e7e556a7e567f0deafeb7167e
AY-BAMZ <aybamz181@gmail.com>
2023-04-04 00:32:17 +0100

    fixing cashout page revamp

--- src/pages/payment/paymentAndWithdrawalBody.jsx
+++ src/pages/payment/paymentAndWithdrawalBody.jsx
@@ -1,49 +1,52 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import MoneyIcon from "../../assets/naira.svg";
-import styles from "../../styles/components/paymentpage.module.css"
+import styles from "../../styles/components/paymentpage.module.css";
 import { fundWallet } from "../api/payment/fundWallet";
 import { getProfile } from "../api/profile/getProfile";
 import Loading from "../components/loading";
 
 function PaymentAndWithdrawalBody() {
   const [operation, setOperation] = useState("deposit");
-  const [depositAmount, setDepositAmount] = useState(null)
-  const [withdrawAmount, setWithdrawAmount] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
+  const [depositAmount, setDepositAmount] = useState(null);
+  const [withdrawAmount, setWithdrawAmount] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleOperation = (e) => {
     setOperation(e.target.value);
   };
 
-  const myDeposit = new Intl.NumberFormat().format(depositAmount)
-  const myWithdrawal = new Intl.NumberFormat().format(withdrawAmount)
+
+  const myDeposit = new Intl.NumberFormat().format(depositAmount);
+  const myWithdrawal = new Intl.NumberFormat().format(withdrawAmount);
 
   const [balance, setBalance] = useState("");
 
   useEffect(() => {
     (async () => {
-    setIsLoading(true)
-    const res = await getProfile();
+      setIsLoading(true);
+      const res = await getProfile();
       setBalance(new Intl.NumberFormat().format(res.walletBalance));
-        setIsLoading(false)
-      })();
+      setIsLoading(false);
+    })();
   }, []);
 
   const handleDeposit = async () => {
-    const res = await fundWallet({depositAmount})
-    const newWindow = window.open(res, '_blank', 'noopener,noreferrer')
-  if (newWindow) newWindow.opener = null
-  }
+    const res = await fundWallet({ depositAmount });
+    const newWindow = window.open(res, "_blank", "noopener,noreferrer");
+    if (newWindow) newWindow.opener = null;
+  };
 
   if (isLoading) {
-    return <Loading />
+    return <Loading />;
   }
 
   return (
     <div className={styles.payment}>
       <h3>Deposit Withdrawal</h3>
-      <h5>Balance: <span>₦{balance}</span></h5>
+      <h5>
+        Balance: <span>₦{balance}</span>
+      </h5>
       <div className={styles.container}>
         <div className={styles.type}>
           <div className={styles.one}>
