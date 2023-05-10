095a567e2265ba4c40aa4ac9d2807d439617d6af
AY-BAMZ <aybamz181@gmail.com>
2023-05-10 10:57:59 +0100

    fixing share voucher issue

--- src/pages/voucher/vouchersLists.jsx
+++ src/pages/voucher/vouchersLists.jsx
@@ -2,22 +2,7 @@ import React, { useState } from "react";
 import styles from "../../styles/components/onevoucher.module.css";
 import SearchIcon from "../../assets/search.svg";
 import { MdContentCopy } from "react-icons/md";
-import { FaShare } from "react-icons/fa";
-import { EmailIcon, FacebookIcon, LinkedinIcon, TelegramIcon, TwitterIcon, WhatsappIcon } from "react-share";
-import Modal from "react-modal";
-import { GrClose } from "react-icons/gr";
-
-const  customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    boxShadow: "0px 0px 20px 10px #55555525",
-  },
-};
+import { RWebShare } from "react-web-share";
 
 function VouchersLists({ data, notify }) {
   const [tab, setTab] = useState(1);
@@ -34,23 +19,6 @@ function VouchersLists({ data, notify }) {
       setFilteredData([]);
     }
   };
-
-  const [modalIsOpen, setIsOpen] = React.useState(false);
-
-  let subtitle;
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = "#000";
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
 
   const copyToClipBoard = async (copyMe) => {
     try {
@@ -132,43 +100,21 @@ function VouchersLists({ data, notify }) {
                 >
                   <MdContentCopy /> copy
                 </td>
-                <td onClick={openModal}>
-                  <FaShare /> share
+                <td>
+                  {/* <FaShare /> share */}
+                  <RWebShare
+                  className={styles.shareModal}
+                    data={{
+                      text: `Someone is sharing a voucher ${voucher.couponCode} for you to cash. Go to pays.co to cash it.`,
+                      url: "https://pays.co/cashout",
+                      title: "Pays Share Voucher",
+                    }}
+                    sites={["facebook", "twitter", "whatsapp", "linkedin"]}
+                    disableNative
+                  >
+                    <div>Share 🔗</div>
+                  </RWebShare>
                 </td>
-        <Modal
-          isOpen={modalIsOpen}
-          onAfterOpen={afterOpenModal}
-          onRequestClose={closeModal}
-          style={customStyles}
-          className={styles.modal}
-          contentLabel="Example Modal"
-        >
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <h3 style={{marginRight: 50}} ref={(_subtitle) => (subtitle = _subtitle)}>
-              Share Voucher code
-            </h3>
-            <GrClose onClick={closeModal} style={{ fontSize: 24 }} />
-          </div>
-          <br />
-          <TwitterIcon size={48} round={true} />
-          <FacebookIcon url={`www.facebook.com`}  quote={`${voucher.couponCode}`} size={48} round={true} />
-          <LinkedinIcon size={48} round={true} />
-          <EmailIcon   size={48} round={true} />
-          <WhatsappIcon size={48} round={true} />
-          <TelegramIcon size={48} round={true} />
-          {/* <div className={styles.buttons}>
-            <br />
-            <button style={{opacity: 0}}>close</button>
-            <button  style={{ background: "#008000", marginRight: 16 }}>Reset</button>
-            <button >Apply</button>
-          </div> */}
-        </Modal>
               </tr>
             ))}
           </tbody>
