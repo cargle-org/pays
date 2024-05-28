3f007b4ecc3790a2cf196de362503ac91c48ec68
Remy Arinze <remyarinze@gmail.com>
2024-05-28 11:25:17 +0100

    chore: made some ui changes along with fixing the redirection bug

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-const Header = dynamic(() => import('../components/header/header'), { ssr: false });
+const Header = dynamic(() => import("../components/header/header"), {
+	ssr: false,
+});
 import styles from "../../styles/Home.module.css";
 import { HiArrowUpRight } from "react-icons/hi2";
 import { useRouter } from "next/router";
@@ -10,70 +12,74 @@ import Footer from "../components/footer/footer";
 import dynamic from "next/dynamic";
 
 function HomePage() {
-  const router = useRouter();
-  const token = getToken();
+	const router = useRouter();
+	const token = getToken();
 
-  const handleUser = () => {
-    if (token) {
-      router.push("/createvouchers");
-    } else {
-      router.push("/login");
-    }
-  };
-  return (
-    <div className={styles.homePage}>
-      <Header />
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.row}>
-            <div className={styles.colOne}>
-              <div>
-
-              <h1>
-                Create gift vouchers, <br />
-                <span> Redeem gifts in cash</span>
-              </h1>
-              <h5>
-                With Usepays.co, you can design and personalize your gift
-                vouchers, and the recipient can choose to cash them out for
-                their value. Our platform offers a simple and hassle-free way to
-                cash out your gift vouchers and use the cash for anything you
-                want.
-              </h5>
-              <div className={styles.buttons}>
-                <div className={styles.btn} onClick={handleUser}>
-                Start Here
-                  <div className={styles.icon}>
-                    <HiArrowUpRight />
-                  </div>
-                </div>
-                <div
-                  onClick={() => router.push("/cashout")}
-                  className={styles.btnTwo}
-                >
-                  Cash a gift
-                  <div className={styles.icon}>
-                    <HiArrowUpRight />
-                  </div>
-                </div>
-              </div>
-              <img src="https://res.cloudinary.com/dmixz7eur/image/upload/v1683798079/chike/Group_8_v3ntku.png" alt="" />
-              </div>
-            </div>  
-            <div className={styles.colTwo}>
-              <img
-                src="https://res.cloudinary.com/dmixz7eur/image/upload/v1681125297/Group_10_zu4ygs.png"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      <Metrics />
-      <Subscribe />
-      <Footer />
-    </div>
-  );
+	const handleUser = () => {
+		if (token) {
+			router.push("/createvouchers");
+		} else {
+			router.push("/login");
+		}
+	};
+	return (
+		<div className={styles.homePage}>
+			<Header />
+			<div className={styles.container}>
+				<div className={styles.content}>
+					<div className={styles.row}>
+						<div className={styles.colOne}>
+							<div>
+								<h1>
+									Create gift vouchers, <br />
+									<span> Redeem gifts in cash</span>
+								</h1>
+								<br />
+								<p>
+									With Usepays.co, you can design and personalize your gift
+									vouchers, and the recipient can choose to cash them out for
+									their value. Our platform offers a simple and hassle-free way
+									to cash out your gift vouchers and use the cash for anything
+									you want.
+								</p>
+								<br />
+								<div className={styles.buttons}>
+									<div className={styles.btn} onClick={handleUser}>
+										Start Here
+										<div className={styles.icon}>
+											<HiArrowUpRight />
+										</div>
+									</div>
+									<div
+										onClick={() => router.push("/cashout")}
+										className={styles.btnTwo}
+									>
+										Cash a gift
+										<div className={styles.icon}>
+											<HiArrowUpRight />
+										</div>
+									</div>
+								</div>
+								<img
+									src="https://res.cloudinary.com/dmixz7eur/image/upload/v1683798079/chike/Group_8_v3ntku.png"
+									alt=""
+								/>
+							</div>
+						</div>
+						<div className={styles.colTwo}>
+							<img
+								src="https://res.cloudinary.com/dmixz7eur/image/upload/v1681125297/Group_10_zu4ygs.png"
+								alt=""
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Metrics />
+			<Subscribe />
+			<Footer />
+		</div>
+	);
 }
 
 export default HomePage;
