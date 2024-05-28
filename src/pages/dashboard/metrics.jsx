e2cbdb08f40ad4858163b09d330abdf1e446c062
Remy Arinze <remyarinze@gmail.com>
2024-05-28 21:02:59 +0100

    chore: fixed redirection bug in fund wallet and forgot password, did ui changes in landingpage, and dashboard

--- src/pages/dashboard/metrics.jsx
+++ src/pages/dashboard/metrics.jsx
@@ -1,52 +1,53 @@
-import React, { useContext, useEffect, useState } from 'react'
-import styles from "../../styles/components/maindashboardpage.module.css"
-import { getProfile } from '../api/profile/getProfile';
-import { getUserDetails } from '../api/auth/auth';
+import React, { useContext, useEffect, useState } from "react";
+import styles from "../../styles/components/maindashboardpage.module.css";
+import { getProfile } from "../api/profile/getProfile";
+import { getUserDetails } from "../api/auth/auth";
 
 function Metrics() {
+	const [userData, setData] = useState("");
+	const [bal, setBalance] = useState("**");
+	const [totalVouchers, setTotalVoucherse] = useState("**");
+	const [amtCashed, setAmtCashed] = useState("**");
+	const [activeVouchers, setActiveVouchers] = useState("**");
+	const [cashedVouchers, setCashedVouchers] = useState("**");
 
-    const [userData, setData] = useState("")
-    const [bal, setBalance] = useState('**')
-    const [totalVouchers, setTotalVoucherse] = useState('**')
-    const [amtCashed, setAmtCashed] = useState('**')
-    const [activeVouchers, setActiveVouchers] = useState('**')
-    const [cashedVouchers, setCashedVouchers] = useState('**')
+	useEffect(() => {
+		(async () => {
+			const res = await getProfile();
+			setBalance(
+				new Intl.NumberFormat().format(res.walletBalance && res.walletBalance)
+			);
+			setTotalVoucherse(res.totalVouchers);
+			setAmtCashed(res.totalAmountCashed);
+			setActiveVouchers(res.activeVouchers);
+			setCashedVouchers(res.cashedVouchers);
+		})();
+	}, []);
 
-    useEffect(() => {
-        (async () => {
-            const res = await getProfile();
-            setBalance(new Intl.NumberFormat().format(res.walletBalance && res.walletBalance));
-            setTotalVoucherse(res.totalVouchers);
-            setAmtCashed(res.totalAmountCashed);
-            setActiveVouchers(res.activeVouchers);
-            setCashedVouchers(res.cashedVouchers);
-          })();
-      }, []);
-
-  return (
-    <div className={styles.metrics}>
-    <div className={styles.card}>
-        <h6>Account Balance</h6>
-        <h2>₦{bal}</h2>
-    </div>
-        <div className={styles.card}>
-            <h6>Total Vouchers</h6>
-            <h2>{totalVouchers}</h2>
-        </div>
-        <div className={styles.card}>
-            <h6>Amount Cashed</h6>
-            <h2>₦{amtCashed}</h2>
-        </div>
-        <div className={styles.card}>
-            <h6>Active Vouchers</h6>
-            <h2>{activeVouchers}</h2>
-        </div>
-        <div className={styles.card}>
-            <h6>Cashed vouchers</h6>
-            <h2>{cashedVouchers}</h2>
-        </div>
-    </div>
-  )
+	return (
+		<div className={styles.metrics}>
+			<div className={styles.card}>
+				<h6>Account Balance</h6>
+				<h4>₦{bal}</h4>
+			</div>
+			<div className={styles.card}>
+				<h6>Total Vouchers</h6>
+				<h4>{totalVouchers}</h4>
+			</div>
+			<div className={styles.card}>
+				<h6>Amount Cashed</h6>
+				<h4>₦{amtCashed}</h4>
+			</div>
+			<div className={styles.card}>
+				<h6>Active Vouchers</h6>
+				<h4>{activeVouchers}</h4>
+			</div>
+			<div className={styles.card}>
+				<h6>Cashed vouchers</h6>
+				<h4>{cashedVouchers}</h4>
+			</div>
+		</div>
+	);
 }
 
-export default Metrics
+export default Metrics;
--- src/pages/home/metrics.jsx
+++ src/pages/home/metrics.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from "react";
 import styles from "../../styles/Home.module.css";
 
 function Metrics() {
-  return (
-    <div className={styles.metrics}>
-        <div className={styles.container}>
-            <div className={styles.row}>
-                <div className={styles.metric}>
-                    <h5>Active users</h5>
-                    <div className={styles.display}>50k +</div>
-                </div>
-                <div className={styles.metric}>
-                    <h5>Vouchers Cashed</h5>
-                    <div className={styles.display}>250k +</div>
-                </div>
-                <div className={styles.metric}>
-                    <h5>Amount Cashed</h5>
-                    <div className={styles.display}>1.2b +</div>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
+	return (
+		<div className={styles.metrics}>
+			<div className={styles.container}>
+				<div className={styles.row}>
+					<div className={styles.metric}>
+						<div className={styles.display}>50k +</div>
+						<p>Active users</p>
+					</div>
+					<div className={styles.metric}>
+						<div className={styles.display}>250k +</div>
+						<p>Vouchers Cashed</p>
+					</div>
+					<div className={styles.metric}>
+						<div className={styles.display}>1.2b +</div>
+						<p>Amount Cashed</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Metrics
+export default Metrics;
